850ed72ca71cd42bc50e6318e18f464a8402e99e
Yasmin-Carloto <yasmincarlotobezerra17@gmail.com>
2024-06-25 15:51:31 -0300

    refactor: avoind code repetition in the features alternate

--- scripts/script.js
+++ scripts/script.js
@@ -13,46 +13,43 @@ const featureExplanatonContainer = document.getElementById("feature-explanation-
 const questionDividerFeature = document.getElementById("question-divider-feature")
 const gamificationFeature = document.getElementById("gamification-feature")
 
-gamificationFeature.addEventListener("click", (e) => {
-    e.preventDefault()
+const updateFeatureExplanation = (imgSrc, imgAlt, titleText, paragraphText) => {
     featureExplanatonContainer.innerHTML = ""
 
     const img = document.createElement("img")
-    img.src = "/images/illustration-features-tab-3.svg"
-    img.alt = "An illustration of the feature"
+    img.src = imgSrc
+    img.alt = imgAlt
     img.id = "feature-image"
 
     const title = document.createElement("h2")
-    title.innerText = "Be competitive"
+    title.innerText = titleText
     title.className = "section-title"
 
     const paragraph = document.createElement("p")
-    paragraph.innerHTML = "With the gamification feature, people studying the same topics will be classified based on the number of answers they answered and the number of points each question has."
+    paragraph.innerHTML = paragraphText
     paragraph.className = "section-description"
 
     featureExplanatonContainer.appendChild(img)
     featureExplanatonContainer.appendChild(title)
     featureExplanatonContainer.appendChild(paragraph)
+};
+
+gamificationFeature.addEventListener("click", (e) => {
+    e.preventDefault()
+    updateFeatureExplanation(
+        "/images/illustration-features-tab-3.svg",
+        "An illustration of the feature",
+        "Be competitive",
+        "With the gamification feature, people studying the same topics will be classified based on the number of answers they answered and the number of points each question has."
+    )
 })
 
 questionDividerFeature.addEventListener("click", (e) => {
     e.preventDefault()
-    featureExplanatonContainer.innerHTML = ""
-
-    const img = document.createElement("img")
-    img.src = "/images/illustration-features-tab-2.svg"
-    img.alt = "An illustration of the feature"
-    img.id = "feature-image"
-
-    const title = document.createElement("h2")
-    title.innerText = "Learn by yourself"
-    title.className = "section-title"
-
-    const paragraph = document.createElement("p")
-    paragraph.innerHTML = "We use a technique inspired by the CBL framework. This characteristic specifies that, when you have a study topic, you, due to your own doubts about the topic, come up with questions about it that must be answered. According to your answer, you will be ranked among other people studying this subject."
-    paragraph.className = "section-description"
-
-    featureExplanatonContainer.appendChild(img)
-    featureExplanatonContainer.appendChild(title)
-    featureExplanatonContainer.appendChild(paragraph)
+    updateFeatureExplanation(
+        "/images/illustration-features-tab-2.svg",
+        "An illustration of the feature",
+        "Learn by yourself",
+        "We use a technique inspired by the CBL framework. This characteristic specifies that, when you have a study topic, you, due to your own doubts about the topic, come up with questions about it that must be answered. According to your answer, you will be ranked among other people studying this subject."
+    )
 })
