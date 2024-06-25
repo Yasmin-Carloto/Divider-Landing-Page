952af6f66630cf018c899db543cff886689b6be9
Yasmin-Carloto <yasmincarlotobezerra17@gmail.com>
2024-06-25 18:11:01 -0300

    feat: creating app's faq section

--- scripts/script.js
+++ scripts/script.js
@@ -53,3 +53,27 @@ questionDividerFeature.addEventListener("click", (e) => {
         "We use a technique inspired by the CBL framework. This characteristic specifies that, when you have a study topic, you, due to your own doubts about the topic, come up with questions about it that must be answered. According to your answer, you will be ranked among other people studying this subject."
     )
 })
+
+const accordionsButtonFaq = document.getElementsByClassName("faq-accordion")
+
+for(let i = 0; i< accordionsButtonFaq.length; i++){
+    accordionsButtonFaq[i].addEventListener("click", (e) => {
+        e.preventDefault()
+        accordionsButtonFaq[i].classList.toggle("active")
+        accordionsButtonFaq[i].parentElement.classList.toggle("active")
+
+        const iconArrow = accordionsButtonFaq[i].childNodes[3]
+        if(accordionsButtonFaq[i].childNodes[3].className == "fa-solid fa-chevron-down"){
+            iconArrow.className = "fa-solid fa-chevron-up"
+        }else{
+            iconArrow.className = "fa-solid fa-chevron-down"
+        }
+
+        let pannel = accordionsButtonFaq[i].nextElementSibling
+        if(pannel.style.display === "block"){
+            pannel.style.display = "none"
+        }else{
+            pannel.style.display = "block"
+        }
+    })
+}
